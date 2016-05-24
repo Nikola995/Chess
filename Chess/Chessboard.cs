9ba037741d13f6cd4eb341aef1254739ba8cf19c
Nikola995 <nikola.spasovski@t-home.mk>
2016-05-24 15:08:33 +0200

    Add basic chessboard

---
+++ Chess/Chessboard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    [Serializable]
+    public class Chessboard
+    {
+        public Color ForWhite { get; set; }
+        public Color ForBlack { get; set; }
+        public int SquareSize { get; set; }
+        public Chessboard ()//default constructor
+        {
+            ForWhite = Color.LemonChiffon;
+            ForBlack = Color.DarkGoldenrod;
+            SquareSize = 60;
+        }
+
+        public int Size()
+        {
+            return SquareSize * 8 + 30; //Size of board + padding from border
+        }
+
+        public void Draw(Graphics g)
+        {
+            Brush whiteBrush = new SolidBrush(ForWhite);
+            Brush blackBrush = new SolidBrush(ForBlack);
+            bool whiteSquare = true;
+            int height = 40;
+            for(int i = 0; i < 8; i++)
+            {
+                int width = 15;
+                for (int j = 0; j < 8; j++)
+                {
+                    if (whiteSquare)
+                    {
+                        g.FillRectangle(whiteBrush, new Rectangle(width, height, SquareSize, SquareSize));
+                    }
+                    else
+                    {
+                        g.FillRectangle(blackBrush, new Rectangle(width, height, SquareSize, SquareSize));
+                    }
+
+                    width += SquareSize;
+                    whiteSquare = !whiteSquare;
+                }
+                height += SquareSize;
+                whiteSquare = !whiteSquare;
+            }
+        }
+    }
+}
---
+++ Chess/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Form1 : Form
+    {
+        public Chessboard chessboard;
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            newGame();
+
+        }
+        private void newGame()
+        {
+            int border = SystemInformation.BorderSize.Height;
+            chessboard = new Chessboard();
+            this.ClientSize = new Size(chessboard.Size() - border, chessboard.Size() + 30 + 17 - border); // Size of form dependent on size of chessboard, for height + 30 for padding from menu in chessboard.Draw() and + 17 for extra padding to stabilize
+            timerPlayer1.Enabled = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            chessboard.Draw(e.Graphics);
+        }
+
+        private void timerPlayer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = this.Height.ToString();
+            toolStripStatusLabel2.Text = this.Width.ToString();
+            toolStripStatusLabel2.Alignment = ToolStripItemAlignment.Right;
+        }
+
+        private void formToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
